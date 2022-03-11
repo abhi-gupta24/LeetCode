56cc72b8632a2adf7293ba2a3dbfc097cf27d694
Abhishek Kumar <59911521+abhi-gupta24@users.noreply.github.com>
2022-03-11 23:02:53 +0530

    Time: 444 ms (28.54%), Space: 10.2 MB (87.11%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,20 +1,22 @@
+//T.C. - O(n^2)  (Brut force appraoch)
+//S.C. - O(1)
+
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) 
     {
         // x + y = target
-        //nums[i] + nums[j] = target
         vector<int>result;
         
         for(int i=0; i<nums.size(); i++)
         {
-            int y = target - nums[i];      // y = target - x
             
             for(int j =i+1 ; j<nums.size(); j++)
             {
-                if( nums[j] == y)
+                    // x + y = target
+                if( nums[i] + nums[j] == target)
                 {
-                    
                     result.push_back(i);  // x
                     result.push_back(j);  // y
                     break;
@@ -27,3 +29,37 @@ public:
         
     }
 };
+
+
+//OR
+
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) 
+//     {
+//         // x + y = target
+//         //nums[i] + nums[j] = target
+//         vector<int>result;
+        
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             int y = target - nums[i];      // y = target - x
+            
+//             for(int j =i+1 ; j<nums.size(); j++)
+//             {
+//                 if( nums[j] == y)
+//                 {
+                    
+//                     result.push_back(i);  // x
+//                     result.push_back(j);  // y
+//                     break;
+//                 }
+//             }
+            
+//         }
+        
+//         return result; 
+        
+//     }
+// };
