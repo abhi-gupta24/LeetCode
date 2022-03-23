34cd78e35a16cd09e1e9393fb43747a393dffc07
Abhishek Kumar <59911521+abhi-gupta24@users.noreply.github.com>
2022-03-23 23:35:29 +0530

    Time: 22 ms (97.12%), Space: 11.6 MB (78.27%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
@@ -1,28 +1,3 @@
-// class Solution {
-// public:
-//     int countHomogenous(string s) 
-//     {
-//          long int count = 0 ;
-//          long  result = 0 ;
-        
-//         for(int i = 0;i < s.size();i++)
-//         { 
-//             if(i> 0 && s[i]==s[i-1] )
-//             {
-//                 count++;
-                
-//             }
-//             else
-//             {
-//                 count = 1;
-//             }
-            
-//             result += count%(10^9 + 7);                       // result = (result + count)%(10^9 + 7);
-//         }
-//       return result;  
-//     }
-// };
-
 
 class Solution {
 public:
@@ -41,5 +16,55 @@ public:
         }
         return ans%M;
     }
-};    
+};  
+
+
+//                      OR
+
+// class Solution {
+// public:
+//     int countHomogenous(string str) {
+        
+//         long long int c=1,t=1;
+        
+//         for(int i=1;i<str.size();i++)
+//         {
+//             if(str[i]==str[i-1])
+//                 t++;
+//             else
+//                 t=1;
+//             c+=t;
+//         }
+//         return (c)%1000000007;
+// 		}
+// 	};
+
+
+
+//                 OR
+
+// class Solution {
+// public:
+//     int countHomogenous(string s) 
+//     {
+//          long  long int count = 1 ;
+//          long long int  result = 1 ;
+        
+//         for(int i = 1;i < s.size();i++)
+//         { 
+//             if( s[i]==s[i-1] )
+//             {
+//                 count++;
+                
+//             }
+//             else
+//             {
+//                 count = 1;
+//             }
+            
+//             result += count;                       // result = (result + count)%(10^9 + 7);
+//         }
+//       return result %(10000000007);  
+//     }
+// };
     
