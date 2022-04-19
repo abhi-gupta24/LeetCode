0976c7d5f087675c7cd977db57d387e36c7b62f5
Abhishek Kumar <59911521+abhi-gupta24@users.noreply.github.com>
2022-04-19 23:21:27 +0530

    Time: 47 ms (21.24%), Space: 19.3 MB (14.12%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) 
+    {
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] != 0)
+            {   
+                nums[count] = nums[i];
+                count ++;
+                
+                // OR
+                // nums[count++] = nums[i];
+            }
+        }
+        
+        for(; count < nums.size(); count++)
+        {
+            nums[count] = 0;
+        }
+        
+    }
+};
